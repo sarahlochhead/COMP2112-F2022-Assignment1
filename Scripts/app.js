27a4f0d59ad6f1d9fd577a43acfd082bba293616
Sarah Lochhead <sarahlochhead@gmail.com>
2022-10-15 19:45:52 -0400

    used jQuery for data fetching

--- Scripts/app.js
+++ Scripts/app.js
@@ -35,8 +35,11 @@
     {
         console.log("App Started");
 
-        LoadData("GET", "./Data/contacts.json", function(XHR){
-            console.log(XHR);
+        // LoadData("GET", "./Data/contacts.json", function(XHR){
+        //     console.log(XHR);
+        // });
+        $.getJSON("./Data/contacts.json", function(DataSource){
+            console.log(DataSource.ContactList[0]);
         });
     }
 
