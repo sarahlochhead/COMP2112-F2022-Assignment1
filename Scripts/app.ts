45c92819b84e3ea20fa5d4d05cce4f3c7a9bb3c0
Sarah Lochhead <sarahlochhead@gmail.com>
2022-10-15 21:08:32 -0400

    updated project files

--- Scripts/app.ts
+++ Scripts/app.ts
@@ -2,42 +2,10 @@
 // IFFE
 (function()
 {
-    /**
-     * Function loads data asynchronously from a URL. 
-     * Calls callback function when the data is done loading
-     *
-     * @param {string} method
-     * @param {string} url
-     * @param {function} callback
-     */
-    function LoadData(method: string , url: string, callback: Function):void
-    {
-        // create object
-        let XHR = new XMLHttpRequest();
-
-        // request
-        XHR.open(method, url);
-
-        // send request
-        XHR.send();
-
-        // event listener
-        XHR.addEventListener("readystatechange", function(){
-            if((XHR.status == 200) && (XHR.readyState == 4))
-            {
-                callback(XHR.responseText); 
-            }
-
-        });
-    }
-
     function Start()
     {
         console.log("App Started");
 
-        // LoadData("GET", "./Data/contacts.json", function(XHR){
-        //     console.log(XHR);
-        // });
         $.getJSON("./Data/contacts.json", function(DataSource){
             console.log(DataSource.ContactList[0]);
         });
